bc0af545bd0334afcb839e1788ff58c6f348829e
Ben Mercer <ben@henroben.co.uk>
2017-03-31 11:20:05 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+"use strict";
+
+console.log('Starting App');
+
+const fs = require('fs');
+const os = require('os');
+
+let user = os.userInfo();
+
+fs.appendFileSync('greetings.txt', `Hello ${user.username}! `);
